cc9b8a6279b1e9c599c318acb01df04387e64bdc
Marco Rodriguez <cv5233@innovaccion.mx>
2022-03-30 15:51:02 -0600

    cambio en el diseño

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(){
     iniciarApp();
-    mediosContacto();
 });
 
 function iniciarApp(){
-    carrousel();
     eventListeners();
     
 }
@@ -27,53 +25,53 @@ function navegacionResponsive(){
     }
 }
 
-function carrousel(){
-    const seleccion = document.querySelector('.carrousel-items');
-    if(seleccion){
-        new Glider(seleccion,{
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: '#indicadores1',
-            arrows: {
-                prev: '#anterior1',
-                next: '#siguiente1'}
-        });
-    }
-}
+// function carrousel(){
+//     const seleccion = document.querySelector('.carrousel-items');
+//     if(seleccion){
+//         new Glider(seleccion,{
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             dots: '#indicadores1',
+//             arrows: {
+//                 prev: '#anterior1',
+//                 next: '#siguiente1'}
+//         });
+//     }
+// }
 
-function mediosContacto(){
-    const imagenVista = [];
-    const tituloMedio=document.querySelector('#titulo-medio');
-    const textoMedio=document.querySelector('#texto-medio');
-    const enlaceMedio=document.querySelector('#enlace-medio');
-    setInterval(() => {
-        for (let $i = 0; $i <= 3; $i++) {
-            imagenVista[$i] = document.querySelector(`*[data-index="${$i}"]`);
-        }
-        if(imagenVista[0].classList.contains('active')){
-            tituloMedio.textContent='titulo 1';
-            textoMedio.textContent='texto 1';
-            enlaceMedio.textContent='enlace 1';
-            enlaceMedio.href='enlace 1';
-        }
-        if(imagenVista[1].classList.contains('active')){
-            tituloMedio.textContent='titulo 2';
-            textoMedio.textContent='texto 2';
-            enlaceMedio.textContent='enlace 2';
-            enlaceMedio.href='enlace 2';
-        }
-        if(imagenVista[2].classList.contains('active')){
-            tituloMedio.textContent='titulo 3';
-            textoMedio.textContent='texto 3';
-            enlaceMedio.textContent='enlace 3';
-            enlaceMedio.href='enlace 3';
-        }
-        if(imagenVista[3].classList.contains('active')){
-            tituloMedio.textContent='titulo 4';
-            textoMedio.textContent='texto 4';
-            enlaceMedio.textContent='enlace 4';
-            enlaceMedio.href='enlace 4';
-        }
-    }, 10);
+// function mediosContacto(){
+//     const imagenVista = [];
+//     const tituloMedio=document.querySelector('#titulo-medio');
+//     const textoMedio=document.querySelector('#texto-medio');
+//     const enlaceMedio=document.querySelector('#enlace-medio');
+//     setInterval(() => {
+//         for (let $i = 0; $i <= 3; $i++) {
+//             imagenVista[$i] = document.querySelector(`*[data-index="${$i}"]`);
+//         }
+//         if(imagenVista[0].classList.contains('active')){
+//             tituloMedio.textContent='titulo 1';
+//             textoMedio.textContent='texto 1';
+//             enlaceMedio.textContent='enlace 1';
+//             enlaceMedio.href='enlace 1';
+//         }
+//         if(imagenVista[1].classList.contains('active')){
+//             tituloMedio.textContent='titulo 2';
+//             textoMedio.textContent='texto 2';
+//             enlaceMedio.textContent='enlace 2';
+//             enlaceMedio.href='enlace 2';
+//         }
+//         if(imagenVista[2].classList.contains('active')){
+//             tituloMedio.textContent='titulo 3';
+//             textoMedio.textContent='texto 3';
+//             enlaceMedio.textContent='enlace 3';
+//             enlaceMedio.href='enlace 3';
+//         }
+//         if(imagenVista[3].classList.contains('active')){
+//             tituloMedio.textContent='titulo 4';
+//             textoMedio.textContent='texto 4';
+//             enlaceMedio.textContent='enlace 4';
+//             enlaceMedio.href='enlace 4';
+//         }
+//     }, 10);
     
-}
+// }
